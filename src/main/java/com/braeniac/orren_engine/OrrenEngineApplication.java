5980146f996bad8853f1df6315dfdeeec4a97d19
Maninder Singh <manny.singh.live@gmail.com>
2026-02-05 14:21:13 -0500

    intialize java spring boot project

---
+++ src/main/java/com/braeniac/orren_engine/OrrenEngineApplication.java
@@ -0,0 +1,13 @@
+package com.braeniac.orren_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrrenEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrrenEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/braeniac/orren_engine/OrrenEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.braeniac.orren_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrrenEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
